4973729e57381760ebfab14d4c68c1e1ec45df36
Andres <andresalejandroninoaraujo@gmail.com>
2025-03-14 10:34:17 -0500

    correcion a figuras

--- Semestre02/co.edu.uptcsoft.claseAbstracta/src/GUI/GuiFigura.java
+++ Semestre02/co.edu.uptcsoft.claseAbstracta/src/GUI/GuiFigura.java
@@ -20,37 +20,38 @@ public class GuiFigura {
                 System.out.println("Saliendo del programa");
                 break;
             }
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingrese el radio del circulo");
+                    String radio = sc.nextLine();
+                    System.out.println(control.interfazCirculo(radio));
+                    break;
+                case 2:
+                    System.out.println("Ingrese el largo del rectangulo");
+                    String largo = sc.nextLine();
+                    System.out.println("Ingrese el ancho del rectangulo");
+                    String ancho = sc.nextLine();
+                    System.out.println(control.interfazRectangulo(largo, ancho));
+                    break;
+                case 3:
+                    System.out.println("Ingrese la base del triangulo");
+                    String base = sc.nextLine();
+                    System.out.println("Ingrese la altura del triangulo");
+                    String altura = sc.nextLine();
+                    System.out.println("Ingrese el lado 1 del triangulo");
+                    String l1 = sc.nextLine();
+                    System.out.println("Ingrese el lado 2 del triangulo");
+                    String l2 = sc.nextLine();
+                    System.out.println("Ingrese el lado 3 del triangulo");
+                    String l3 = sc.nextLine();
+                    System.out.println(control.interfazTriangulo(base, altura, l1, l2, l3));
+                default:
+                    System.out.println("Opcion no valida. Intente de nuevo");
+                    continue;
+            }
         }
 
-        switch (opcion){
-            case 1:
-                System.out.println("Ingrese el radio del circulo");
-                String radio = sc.nextLine();
-                System.out.println(control.interfazCirculo(radio));
-                break;
-            case 2:
-                System.out.println("Ingrese el largo del rectangulo");
-                String largo = sc.nextLine();
-                System.out.println("Ingrese el ancho del rectangulo");
-                String ancho = sc.nextLine();
-                System.out.println(control.interfazRectangulo(largo, ancho));
-                break;
-            case 3:
-                System.out.println("Ingrese la base del triangulo");
-                String base = sc.nextLine();
-                System.out.println("Ingrese la altura del triangulo");
-                String altura = sc.nextLine();
-                System.out.println("Ingrese el lado 1 del triangulo");
-                String l1 = sc.nextLine();
-                System.out.println("Ingrese el lado 2 del triangulo");
-                String l2 = sc.nextLine();
-                System.out.println("Ingrese el lado 3 del triangulo");
-                String l3 = sc.nextLine();
-                System.out.println(control.interfazTriangulo(base, altura, l1, l2, l3));
-            default:
-                System.out.println("Opcion no valida. Intente de nuevo");
-                continue;
-        }
+
 
     }
 }
